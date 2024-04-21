2dc706bc82a597a6fe2cdf5e74fa7d2fc854be57
SHRADDHA SONAWANE <shraddhasonawane524@gmail.com>
2024-04-21 14:02:35 +0530

    allchange4

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,31 +1,9 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import '../components/student.css';
+import React from 'react'
 
 const Student = () => {
-  const absentStudents = 5; // Assuming there are 5 absent students
-  const presentStudents = 15; // Assuming there are 15 present students
-
   return (
-    <div>
-      <h2>Student</h2>
-      <div className="card-container">
-        <Card className="card">
-          <CardContent>
-            <h3>Absent Students</h3>
-            <p>{absentStudents} {absentStudents === 1 ? 'student' : 'students'} absent</p>
-          </CardContent>
-        </Card>
-        <Card className="card">
-          <CardContent>
-            <h3>Present Students</h3>
-            <p>{presentStudents} {presentStudents === 1 ? 'student' : 'students'} present</p>
-          </CardContent>
-        </Card>
-      </div>
-    </div>
-  );
-};
+    <div>Student</div>
+  )
+}
 
-export default Student;
+export default Student
